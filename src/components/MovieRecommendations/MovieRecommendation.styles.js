c6c433c7da2a0d3d2a18a047b37730fe763a6a4a
Trần Việt Hà <127063257+hiimbias@users.noreply.github.com>
2025-02-28 13:18:50 +0700

    Updates

--- src/components/MovieRecommendations/MovieRecommendation.styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { IMAGE_BASE_URL } from '../../config';
-
-export const Wrapper = styled.div``;
-export const Content = styled.div``;
-export const Text = styled.div``;
